f85f920d0ad9ccc0df81ee28d9c79a36b8118cae
leviacerman123 <dapergnat@gmail.com>
2025-04-25 18:59:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+//exam xudoyberdiyev aziz
+//1-oydan 2 oygacha 
+
+
+// 1-⁡⁣⁣⁢savol⁡
+
+// let prom = alert(5 % 2)
+
+// 2-⁡⁢⁣⁢savol⁡
+
+// alert(Math.random('0,1,2,3,4,5,6,7,8,9,10'));  
+
+// 3-⁡⁢⁢⁣savol⁡
+
+// alert(Math.trunc(12.510));     
+
+// 4-⁡⁣⁢⁣savol⁡ 
+
+// let text = (`MARS IT SCHOOL`)
+
+// alert(text.length);
+
+// 5-⁡⁣⁣⁢savol⁡
+
+
+
+
+// for (let index = 0; index  <=10 ; index++) {
+//     console.log(`MARS IT SCHOOL`);
+
+
+// }
+
+
+// 6-⁡⁢⁢⁢savol⁡
+
+// let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+
+
+// 7-⁡⁢⁣⁣savol⁡
+
+// let proverka = +prompt(`yosh kiriting`)
+
+// if (proverka == 18) {
+//     console.log(`siz balogat yoshiga yetgansiz`);
+    
+// } else  {
+//     console.log(`siz balogat yoshiga yetmagansiz`);
+    
+// } 
+
+
+
+
+
+
+// 9-savol 
+
+// html faylga qarang  
+
+
+
+
+// 10-savol
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < sonlar.length; i++) {
+//   if (sonlar[i] % 2 === 0) {
+//     console.log(sonlar[i]);
+//   }
+// }
+
